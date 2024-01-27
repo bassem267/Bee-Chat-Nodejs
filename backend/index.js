4042108734320435984af93309d8ab3ea627755b
Bassem Ismail <bassem8mohamed8@gmail.com>
2024-01-27 03:39:22 +0200

    first commit

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+  })
+);
+
+app.post("/auth", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "c22099f4-2f0e-4783-90c8-d5fe4761aafb" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3002);
